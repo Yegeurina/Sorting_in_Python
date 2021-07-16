0b39fe5e24e0c1a20fb4fe7710d830108ef248b0
Yegeurina <yegeurinasec@gmail.com>
2021-07-17 00:34:56 +0900

    Sorted() in Python

---
+++ SortInPython.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import cProfile
+import psutil
+
+def memory_usage() :
+    p=psutil.Process();
+    rss = p.memory_info().rss / 2 ** 20 #Byte to MB
+    print(f"memory usage : {rss:10.5f}MB")
+
+def pythonSort(array) :
+    return sorted(array)
+
+
+if __name__ == '__main__':
+
+    data = pd.Cov = pd.read_csv('./Random.csv')
+    Random = list(data['0'])
+    data = pd.Cov = pd.read_csv('./Sorted.csv')
+    Sorted = list(data['0'])
+    data = pd.Cov = pd.read_csv('./Reversed.csv')
+    Reversed = list(data['0'])
+
+    memory_usage()
+    cProfile.run('pythonSort(Random)')
+    memory_usage()
+    cProfile.run('pythonSort(Sorted)')
+    memory_usage()
+    cProfile.run('pythonSort(Reversed)')
+    memory_usage()
